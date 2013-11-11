ddc7c1606ac956464a3fdf53ea84132df3633fa4
scige <panf06@gmail.com>
2013-11-11 20:46:07 +0800

    Second

--- src/com/example/github/MainActivity.java
+++ src/com/example/github/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
 
@@ -17,6 +16,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        setContentView(R.layout.activity_main);
         return true;
     }
     
